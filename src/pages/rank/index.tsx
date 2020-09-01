10f0047c1c5fa4218111c586638fa7ec6576141e
liuyan <450995596@qq.com>
2020-09-01 21:06:01 +0800

    ✨feat(rank): 添加store

--- src/pages/rank/index.tsx
+++ src/pages/rank/index.tsx
@@ -3,11 +3,22 @@ import Scroll from '../../baseUI/scroll'
 import './index.scss'
 
 const Rank:React.FC = ()=>{
+
+
+    const renderRankList = ()=>{
+        return (
+            <div className='list'>
+
+            </div>
+        )
+    }
+
     return (
         <div className='rank-container'>
             <Scroll>
                 <div>
-                    22
+                <h1 className="offical" >官方榜</h1>
+                <h1 className="global" >全球榜</h1>
                 </div>
             </Scroll>
         </div>
---
+++ src/pages/rank/store/index.tsx
@@ -0,0 +1,19 @@
+export const CHANGE_RANK_LIST = 'home/rank/CHANGE_RANK_LIST';
+export const CHANGE_LOADING = 'home/rank/CHANGE_LOADING';
+
+const defaultState = {
+    rankList: [],
+}
+
+const reducer = (state = defaultState,action) =>{
+    switch (action.type) {
+        case value:
+            
+            break;
+    
+        default:
+            break;
+    }
+}
+
+export {reducer}
